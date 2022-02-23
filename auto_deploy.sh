95c34e4ae9e349d47928b5b2f37d37c9633bacf3
JimyFengqi <jmps515@163.com>
2022-02-23 14:53:35 +0800

    修改deploy脚本相关内容
    travis使用时长到期，因此更换脚本

---
+++ auto_deploy.sh
@@ -0,0 +1,8 @@
+hexo generate
+git clone -b master git@github.com:JimyFengqi/JimyFengqi.github.io.git .deploy_git
+mv .deploy_git/.git/ ./public/
+cd ./public
+git add .
+git commit -m "Auto update docs by auto_deploy script"
+git push --force --quiet git@github.com:JimyFengqi/JimyFengqi.github.io.git master:master
+git push --force --quiet git@gitee.com:JimyFengqi/JimyFengqi.gitee.io.git master:master
